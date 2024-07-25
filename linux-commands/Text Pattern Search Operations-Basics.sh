c84117130b1f08ac46b47e93edc29be774945d25
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2024-07-25 12:20:33 -0400

    Update and rename Text Pattern Search Operations.sh to Text Pattern Search Operations-Basics.sh

---
+++ linux-commands/Text Pattern Search Operations-Basics.sh
@@ -0,0 +1,34 @@
+# Search by 'Ignoring Word Case'
+grep -i 'error' catalina.out
+
+# Search 'Recursively' for a word in all files within the specific directory
+grep -r 'error' /tomcat/logs
+
+# Display line number of the word in a file
+grep -n 'error' catalina.out
+
+# Show all lines, that doesn't contain the word
+grep -v 'error' catalina.out
+
+# Search only exact match of the word 
+grep -w 'error' catalina.out
+
+# Get the count of a word in the file
+grep -c 'error' catalina.out
+
+# Displays lines after each occurrence of a word
+grep -A20 'error' catalina.out
+
+# Displays lines before each occurrence of a word
+grep -B20 'error' catalina.out
+
+# list the files which has the word in those 
+grep -l 'error' /tomcat/logs
+
+# search lines containing either of the words
+grep -e 'error' -e 'info' catalina.out
+
+# dispaly on the word which match 
+
+
+
