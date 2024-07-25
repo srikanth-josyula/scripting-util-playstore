756884de0b12d7b1c6539e268ada09dfb088728f
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2024-07-25 14:08:08 -0400

    Update advanced-commands.sh

--- linux-commands/Text Pattern Search Operations/advanced-commands.sh
+++ linux-commands/Text Pattern Search Operations/advanced-commands.sh
@@ -1,2 +1,19 @@
-#search with a wildchar
+#search with a wildchar (starts with word and continue any text)
 grep 'error.*' catalina.out
+grep 'PUT /alfresco/api/-default-/public/alfresco/versions/1/nodes.*HTTP/1.1.*200' catalina.out
+
+#search for lines starting and ending with specific pattern
+grep '^2024.*200$' catalina.out
+
+#combine multiple greps
+grep 'error' catalina.out | grep 'SQL'
+
+#matching and counting
+grep 'error' catalina.out | grep 'SQL' | wc-l
+
+#matching and combination
+grep 'error' catalina.out | grep -E 'SQL|lock'
+grep -A2 -B2 'exception' catalina.log | grep -E 'Caused by|Exception'
+
+#pattern matching with json displays all jsons
+grep -oP '{.*?}' catalina.out
